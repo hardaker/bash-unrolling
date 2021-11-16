8784c1ff27908e002e89ba15a31612dddb468ad0
Wes Hardaker <opensource@hardakers.net>
2021-11-16 13:46:37 -0800

    argparse and output switching

--- unroll.py
+++ unroll.py
@@ -1,7 +1,37 @@
+#!/usr/bin/python3
+
 import bashparse
 
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, FileType
+from logging import debug, info, warning, error, critical
+import logging
+import sys
 
-def basic_node_unroll(nodes, function_dict = {}, command_alias_list={}):
+def parse_args():
+        parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter,
+                                description=__doc__,
+                                epilog="Exmaple Usage: ")
+
+        parser.add_argument("--log-level", "--ll", default="info",
+                            help="Define the logging verbosity level (debug, info, warning, error, fotal, critical).")
+
+        parser.add_argument("input_file", type=FileType('r'),
+                            nargs='?', default=sys.stdin,
+                            help="")
+
+        parser.add_argument("output_file", type=FileType('w'),
+                            nargs='?', default=sys.stdout,
+                            help="")
+
+        args = parser.parse_args()
+        log_level = args.log_level.upper()
+        logging.basicConfig(level=log_level,
+                            format="%(levelname)-10s:\t%(message)s")
+        return args
+
+
+def basic_node_unroll(nodes, function_dict = {},
+                      command_alias_list={}, output_stream=sys.stdout):
 	# Command substitutions are gonna be weird
 	if type(nodes) is not list: nodes = [nodes]
 	for node in nodes: 
@@ -10,13 +40,13 @@ def basic_node_unroll(nodes, function_dict = {}, command_alias_list={}):
 
 	for node in nodes: 
 		if node.kind == 'compound':
-			unrolled_nodes += basic_node_unroll(node.list, function_dict, command_alias_list)
+			unrolled_nodes += basic_node_unroll(node.list, function_dict, command_alias_list, output_stream=output_stream)
 		elif node.kind == 'for':
-			print('for node: ', node)
+			output_stream.write('for node: ' + str(node) + "\n")
 			if len(node.parts) > 4:
 				command_nodes = bashparse.return_paths_to_node_type(node.parts[4:], 'command')
 				for command in command_nodes:
-					unrolled_nodes += basic_node_unroll(command.node, function_dict, command_alias_list)
+					unrolled_nodes += basic_node_unroll(command.node, function_dict, command_alias_list, output_stream=output_stream)
 		elif node.kind == 'if':
 			unrolled_nodes += [ node ]
 		elif node.kind == 'command':
@@ -25,7 +55,7 @@ def basic_node_unroll(nodes, function_dict = {}, command_alias_list={}):
 			node = bashparse.replace_functions(node, function_dict)
 			for itr in node:
 				if itr.kind == 'compound': # ie function replacement happened
-					unrolled_nodes += basic_node_unroll(itr.list, function_dict, command_alias_list)
+					unrolled_nodes += basic_node_unroll(itr.list, function_dict, command_alias_list, output_stream=output_stream)
 				else:
 					unrolled_nodes += [ itr ]
 		elif node.kind == 'function':
@@ -36,20 +66,25 @@ def basic_node_unroll(nodes, function_dict = {}, command_alias_list={}):
 			unrolled_nodes += basic_node_unroll(node.list)
 	return unrolled_nodes
 
+def main():
+        args = parse_args()
+        
+        text = args.input_file.read()
+        
+        nodes = bashparse.parse(text)
+        
+        var_list = {}
+        replaced_nodes = bashparse.substitute_variables(nodes, var_list)
+        
+        unrolled_nodes = basic_node_unroll(replaced_nodes,
+                                           output_stream=args.output_file)
+        
+        args.output_file.write('unrolled nodes: ')
+        for ast in unrolled_nodes:
+	            args.output_file.write(ast.dump() + "\n")
 
 
+if __name__ == "__main__":
+        main()
 
-filename = './testing.sh'
 
-text = open(filename).read()
-
-nodes = bashparse.parse(text)
-
-var_list = {}
-replaced_nodes = bashparse.substitute_variables(nodes, var_list)
-
-unrolled_nodes = basic_node_unroll(replaced_nodes)
-
-print('unrolled nodes: ')
-for ast in unrolled_nodes:
-	print(ast.dump())
